4acaa07872fec60b9cd53b0750d79bdf483dbc4c
Samuel Obuobi Lartey <kwesiobuobi7@gmail.com>
2022-09-26 13:32:45 -0700

    Configure Routes for HomeView (without fixing the folder structure)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Homepage from './components/home_page/home_page'
+import HomeView from './components/home_view/home_view'
 
 const App = () => {
   return (
-    <Homepage />
+    <Routes>
+      {/* <Route path="/" element={<HomeView />} /> */}
+      <Route path="/" element={<HomeView />}>
+        <Route index element={<Homepage />} />
+        <Route path="about" element={<h1>About Page</h1>} />
+        <Route path="admissions" element={<h1>Admissions Page</h1>} />
+        <Route path="academics" element={<h1>Academics Page</h1>} />
+        <Route path="gallery" element={<h1>gallery Page</h1>} />
+        <Route path="staff" element={<h1>staff Page</h1>} />
+        <Route path="student" element={<h1>student Page</h1>} />
+        <Route path="contact" element={<h1>contact Page</h1>} />
+      </Route>
+
+      <Route path="/student/login" element={<h1>STUDENT LOGIN PAGE</h1>} />
+      <Route path="/staff/login" element={<h1>STUDENT LOGIN PAGE</h1>} />
+      <Route path="/admin" element={<h1>STUDENT LOGIN PAGE</h1>} />
+    </Routes>
+
   )
 }
 
---
+++ src/components/home_view/home_view.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Footer from '../shared/footer/footer'
+import Header from '../shared/header/header'
+
+const HomeView = () => {
+  return (
+    <>
+        <Header />
+        <Outlet />
+        <Footer />
+    </>
+  )
+}
+
+export default HomeView
