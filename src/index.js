24afd2fd369ecb3823d9d77315d106bebac76643
Samuel Obuobi Lartey <kwesiobuobi7@gmail.com>
2022-09-26 11:35:52 -0700

    Add BrowserRouter to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 // eslint-disable-next-line
 import "swiper/css/bundle";
@@ -12,7 +13,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
