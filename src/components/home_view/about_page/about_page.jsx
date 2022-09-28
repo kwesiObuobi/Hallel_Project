8075e4a34e6efad694c91c5f325ed775f8cea061
Samuel Obuobi Lartey <kwesiobuobi7@gmail.com>
2022-09-28 14:54:50 -0700

    Add history content to 'History' component

--- src/components/home_view/about_page/about_page.jsx
+++ src/components/home_view/about_page/about_page.jsx
@@ -4,11 +4,13 @@ import './about_page.css'
 
 import aboutPageImg from '../../../assets/images/website_images/aboutUs.jpg'
 import PageImg from '../../shared/page_img/page_img'
+import History from './history/history'
 
 const AboutPage = () => {
   return (
     <>
       <PageImg img_url={aboutPageImg} title="About Us" />
+      <History />
     </>
   )
 }
--- src/components/home_view/about_page/history/history.jsx
+++ src/components/home_view/about_page/history/history.jsx
@@ -4,7 +4,28 @@ import './history.css';
 
 const History = () => {
   return (
-    <div>History</div>
+    <section>
+      <h5> • HOW IT ALL BEGAN • </h5>
+      <h2>our history</h2>
+
+      <div className="container history-content">
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra. Quis risus sed vulputate odio ut. Eget est lorem ipsum dolor sit amet. Ut consequat semper viverra nam libero justo. Lorem dolor sed viverra ipsum nunc aliquet. Quis hendrerit dolor magna eget est lorem ipsum dolor. Purus viverra accumsan in nisl nisi. Nec tincidunt praesent semper feugiat nibh sed pulvinar. Consectetur libero id faucibus nisl tincidunt eget nullam. Nulla porttitor massa id neque aliquam vestibulum morbi blandit. Massa sapien faucibus et molestie ac feugiat sed lectus vestibulum. Habitant morbi tristique senectus et netus et. Consequat id porta nibh venenatis cras sed felis eget velit. Facilisi nullam vehicula ipsum a arcu cursus vitae congue. Tortor at auctor urna nunc id cursus metus aliquam. Etiam tempor orci eu lobortis elementum nibh tellus molestie nunc. Tellus in metus vulputate eu scelerisque felis. 
+        </p> <br />
+        <p>
+          Vulputate enim nulla aliquet porttitor lacus luctus. Mauris in aliquam sem fringilla. Urna porttitor rhoncus dolor purus. Pulvinar mattis nunc sed blandit libero volutpat sed. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Dui ut ornare lectus sit amet. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Fringilla urna porttitor rhoncus dolor purus non enim. Velit dignissim sodales ut eu sem integer vitae justo. Lectus arcu bibendum at varius vel. Ultricies lacus sed turpis tincidunt.
+        </p> <br />
+        <p>
+          Lobortis feugiat vivamus at augue eget arcu dictum. Enim sed faucibus turpis in eu mi bibendum neque. Aliquam sem fringilla ut morbi tincidunt augue interdum. Odio facilisis mauris sit amet massa vitae tortor condimentum. Pellentesque habitant morbi tristique senectus et netus et malesuada. Odio ut sem nulla pharetra diam sit amet nisl. Aenean et tortor at risus viverra. Neque laoreet suspendisse interdum consectetur. In hendrerit gravida rutrum quisque non. Maecenas volutpat blandit aliquam etiam erat. Placerat orci nulla pellentesque dignissim enim sit amet. Laoreet non curabitur gravida arcu ac. Integer eget aliquet nibh praesent tristique magna. Libero enim sed faucibus turpis in eu mi. Risus in hendrerit gravida rutrum quisque non tellus orci ac. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque.
+        </p> <br />
+        <p>
+          Scelerisque fermentum dui faucibus in ornare quam viverra orci. Mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing. Sit amet massa vitae tortor condimentum lacinia. Adipiscing tristique risus nec feugiat in fermentum posuere. Pharetra magna ac placerat vestibulum. Facilisis leo vel fringilla est ullamcorper eget nulla facilisi. Facilisis leo vel fringilla est ullamcorper eget nulla facilisi etiam. Tellus in metus vulputate eu scelerisque. Lacinia at quis risus sed vulputate odio ut. Integer feugiat scelerisque varius morbi enim nunc. Mattis nunc sed blandit libero volutpat sed cras ornare. Cras pulvinar mattis nunc sed blandit libero volutpat sed cras. Massa tincidunt nunc pulvinar sapien et ligula ullamcorper. Diam vulputate ut pharetra sit. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet. Morbi tristique senectus et netus et. <br />
+        </p> <br />
+      
+      </div>
+
+    </section>
   )
 }
 
