8c26e8c5d61c2aac84988a1f9397e23aa2c683e0
Samuel Obuobi Lartey <kwesiobuobi7@gmail.com>
2022-09-28 16:10:12 -0700

    Add content to 'MissionVisionValues' component

--- src/components/home_view/about_page/about_page.jsx
+++ src/components/home_view/about_page/about_page.jsx
@@ -5,12 +5,14 @@ import './about_page.css'
 import aboutPageImg from '../../../assets/images/website_images/aboutUs.jpg'
 import PageImg from '../../shared/page_img/page_img'
 import History from './history/history'
+import MissionVisionValues from './mission_vision_values/mission_vision_values'
 
 const AboutPage = () => {
   return (
     <>
       <PageImg img_url={aboutPageImg} title="About Us" />
       <History />
+      <MissionVisionValues />
     </>
   )
 }
--- src/components/home_view/about_page/mission_vision_values/mission_vision_values.jsx
+++ src/components/home_view/about_page/mission_vision_values/mission_vision_values.jsx
@@ -4,7 +4,45 @@ import './mission_vision_values.css';
 
 const MissionVisionValues = () => {
   return (
-    <div>MissionVisionValues</div>
+    // mvv = mission_vision_values
+    // <div className="mvv-container-outer-box">
+    //   <section className="mvv-container">
+    //   <h5> • WHERE WE ARE HEADED • </h5>
+    //   <h2>mission | vision | values</h2>
+    //   </section>
+    // </div>
+
+    
+    <section className="mvv-container">
+      <h5 className="mvv-h5"> • WHERE WE ARE HEADED • </h5>
+      <h2 className="mvv-h2">mission | vision | values</h2>
+
+      <div className="container mvv-sub-heading">
+        <h3 className="mvv-sub-heading-h3">Our Mission</h3>
+        <p className="mvv-sub-heading-p">Vulputate enim nulla aliquet porttitor lacus luctus. Mauris in aliquam sem fringilla. Urna porttitor rhoncus dolor purus.</p>
+      </div>
+
+      <div className="container mvv-sub-heading">
+        <h3 className="mvv-sub-heading-h3">Our Vision</h3>
+        <p className="mvv-sub-heading-p">Vulputate enim nulla aliquet porttitor lacus luctus. Mauris in aliquam sem fringilla. Urna porttitor rhoncus dolor purus.</p>
+      </div>
+
+      <div className="container mvv-sub-heading">
+        <h3 className="mvv-sub-heading-h3">Core Values</h3>
+        <ul className="mvv-sub-heading-ul">
+          <li>Excellence</li>
+          <li>Innovation</li>
+          <li>Integrity</li>
+          <li>Boldness</li>
+          <li>Freedom</li>
+          <li>Adventure</li>
+        </ul>
+      </div>
+      
+
+    </section>
+    
+    
   )
 }
 
