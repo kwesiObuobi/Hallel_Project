4590c8fe1c9ec9eb9bfd28206784a4f865689b8f
Samuel Obuobi Lartey <kwesiobuobi7@gmail.com>
2022-09-28 14:00:38 -0700

    Add 'Anthem' component to 'About us' component

---
+++ src/components/home_view/about_page/anthem/anthem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './anthem.css';
+
+const Anthem = () => {
+  return (
+    <div>Anthem</div>
+  )
+}
+
+export default Anthem
---
+++ src/components/home_view/about_page/mission_vision_values/mission_vision_values.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './mission_vision_values.css';
+
+const MissionVisionValues = () => {
+  return (
+    <div>MissionVisionValues</div>
+  )
+}
+
+export default MissionVisionValues
