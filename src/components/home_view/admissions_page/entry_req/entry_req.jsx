56b840277d512cffd95af7868506c434ecc7ca91
Samuel Obuobi Lartey <kwesiobuobi7@gmail.com>
2022-10-04 13:39:11 -0700

    Complete EntryRequirement section

--- src/components/home_view/admissions_page/entry_req/entry_req.jsx
+++ src/components/home_view/admissions_page/entry_req/entry_req.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 import './entry_req.css';
 
-import lion from '../../../../assets/images/website_images/crest/lion.jpg';
+import kg from '../../../../assets/images/website_images/admissions/kg.jpg';
+import primary1 from '../../../../assets/images/website_images/admissions/primary1.jpg';
+import primary4 from '../../../../assets/images/website_images/admissions/primary4.jpg';
+import jhs from '../../../../assets/images/website_images/admissions/jhs.jpg';
 
 
 const EntryRequirements = () => {
@@ -15,18 +18,61 @@ const EntryRequirements = () => {
             <h3 className="entry-req-item-h3">KG 1 - KG 2</h3>
             <div className="entry-req-item-grid">
                 <div className="entry-req-item-img-box">
-                    <img src={lion} alt="" className="entry-req-item-img" />
+                    <img src={kg} alt="" className="entry-req-item-img" />
                 </div>
                 <ul className="entry-req-item-ul">
                     <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
                     <li className="entry-req-item-li">For KG 2 students, Nulla at volutpat diam ut venenatis tellus in metus. </li>
-
                     <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
                     <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
-                    
                 </ul>
             </div>
         </div>  
+
+        <div className="entry-req-item">
+            <h3 className="entry-req-item-h3">PRIMARY 1-3</h3>
+            <div className="entry-req-item-grid">
+                <div className="entry-req-item-img-box">
+                    <img src={primary1} alt="" className="entry-req-item-img" />
+                </div>
+                <ul className="entry-req-item-ul">
+                    <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
+                    <li className="entry-req-item-li">For KG 2 students, Nulla at volutpat diam ut venenatis tellus in metus. </li>
+                    <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
+                    <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
+                </ul>
+            </div>
+        </div>
+
+        <div className="entry-req-item">
+            <h3 className="entry-req-item-h3">PRIMARY 4-6</h3>
+            <div className="entry-req-item-grid">
+                <div className="entry-req-item-img-box">
+                    <img src={primary4} alt="" className="entry-req-item-img" />
+                </div>
+                <ul className="entry-req-item-ul">
+                    <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
+                    <li className="entry-req-item-li">For KG 2 students, Nulla at volutpat diam ut venenatis tellus in metus. </li>
+                    <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
+                    <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
+                </ul>
+            </div>
+        </div>
+
+        <div className="entry-req-item">
+            <h3 className="entry-req-item-h3">JHS 1-3</h3>
+            <div className="entry-req-item-grid">
+                <div className="entry-req-item-img-box">
+                    <img src={jhs} alt="" className="entry-req-item-img" />
+                </div>
+                <ul className="entry-req-item-ul">
+                    <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
+                    <li className="entry-req-item-li">For KG 2 students, Nulla at volutpat diam ut venenatis tellus in metus. </li>
+                    <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
+                    <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
+                </ul>
+            </div>
+        </div>
 
     </section>
   )
