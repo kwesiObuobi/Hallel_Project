ad374d755531df22f943be1faf6396ce7a48879f
Samuel Obuobi Lartey <kwesiobuobi7@gmail.com>
2022-10-04 12:42:51 -0700

    Convert code of Entry req. item to an inside-component

--- src/components/home_view/admissions_page/entry_req/entry_req.jsx
+++ src/components/home_view/admissions_page/entry_req/entry_req.jsx
@@ -4,24 +4,34 @@ import './entry_req.css';
 
 import lion from '../../../../assets/images/website_images/crest/lion.jpg';
 
-const EntryRequirements = () => {
-  return (
-    <section className="container entry_req-section">
-        <h5 className="entry-req-h5">• GETTING ENROLLED •</h5>
-        <h2 className="entry-req-h2">entry requirements</h2>
-
+const EntryReqItem = () => {
+    return (
         <div className="entry-req-item">
-            <h3 className="entry-req-item-h3">KG1 - KG2</h3>
+            <h3 className="entry-req-item-h3">KG 1 - KG 2</h3>
             <div className="entry-req-item-grid">
                 <div className="entry-req-item-img-box">
                     <img src={lion} alt="" className="entry-req-item-img" />
                 </div>
                 <ul className="entry-req-item-ul">
                     <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
-                    <li className="entry-req-item-li">For KG 2 students, <li>Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li></li>
+                    <li className="entry-req-item-li">For KG 2 students, Nulla at volutpat diam ut venenatis tellus in metus. </li>
+
+                    <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
+                    <li className="entry-req-item-li">Children who will be four years old by September in the year of admission are eligible to apply to KG 1</li>
+                    
                 </ul>
             </div>
         </div>
+    )
+}
+
+const EntryRequirements = () => {
+  return (
+    <section className="container entry-req-section">
+        <h5 className="entry-req-h5">• GETTING ENROLLED •</h5>
+        <h2 className="entry-req-h2">entry requirements</h2>
+
+        <EntryReqItem />        
 
     </section>
   )
--- src/components/home_view/home_page/our_mission/our_mission.jsx
+++ src/components/home_view/home_page/our_mission/our_mission.jsx
@@ -7,7 +7,7 @@ const OurMission = () => {
       <h5> • GET TO KNOW • </h5>
       <h2>our mission</h2>
       <h3 className="mission__h3">We have an explicit mission to prepare entirely <br/>trustworthy, entrepreneurial leaders in Africa;</h3>
-      <p className="mission__p">to raise within students, the critical thinking skills, the concern for others, <br/>and the overall courage it will take to transform the continent positively. <br/>The "better world" starts with us!</p>
+      <p className="mission__p">to raise within students the critical thinking skills, the concern for others, <br/>and the overall courage it will take to transform the continent positively. <br/>The "better world" starts with us!</p>
     </section>
   )
 }
