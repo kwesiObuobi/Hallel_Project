870b0c7ee9175cb8a366cf9a2c2721420de2d90a
Samuel Obuobi Lartey <kwesiobuobi7@gmail.com>
2022-10-04 15:46:35 -0700

    Add OpenAdmissions component under the home carousel

--- src/components/home_view/home_page/home_page.jsx
+++ src/components/home_view/home_page/home_page.jsx
@@ -7,11 +7,13 @@ import SecondImg from '../../home_view/home_page/second_img/second_img';
 import HomeNotifications from '../../home_view/home_page/home_notifications/home_notifications';
 import Testimonials from '../../home_view/home_page/testimonials/testimonials';
 import Newsletter from '../../home_view/home_page/newsletter/newsletter';
+import OpenAdmissions from './open_admissions/open_admissions';
 
 const Homepage = () => {
   return (
     <>
       <HomeCarousel />
+      <OpenAdmissions />
       <OurMission />
       <SecondImg />
       <HomeNotifications />
---
+++ src/components/home_view/home_page/open_admissions/open_admissions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './open_admissions.css';
+
+const OpenAdmissions = () => {
+  return (
+    <div>OpenAdmissions</div>
+  )
+}
+
+export default OpenAdmissions
