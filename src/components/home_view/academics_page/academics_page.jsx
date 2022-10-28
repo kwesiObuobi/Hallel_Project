df5ef75a430c709876de1c0a36d9df7f2c11039b
Samuel Obuobi Lartey <kwesiobuobi7@gmail.com>
2022-10-28 12:32:00 -0700

    Add Learning goals component to academics page

--- src/components/home_view/academics_page/academics_page.jsx
+++ src/components/home_view/academics_page/academics_page.jsx
@@ -4,11 +4,13 @@ import './academics_page.css'
 
 import academicsPageImg from '../../../assets/images/website_images/academics.jpg'
 import PageImg from '../../shared/page_img/page_img'
+import LearningGoals from './learning_goals/learning_goals'
 
 const AcademicsPage = () => {
   return (
     <>
       <PageImg img_url={academicsPageImg} title="Academics" />
+      <LearningGoals />
     </>
   )
 }
---
+++ src/components/home_view/academics_page/learning_goals/learning_goals.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './learning_goals.css';
+
+const LearningGoals = () => {
+  return (
+    <section className="academics__learning-goals-section container">
+        <h5 className="academics__learning-goals-h5">• OUR STUDENTS HAVE THESE •</h5>
+        <h2 className="academics__learning-goals-h2">our 10 learning goals</h2>
+
+        
+
+    </section>
+  )
+}
+
+export default LearningGoals
