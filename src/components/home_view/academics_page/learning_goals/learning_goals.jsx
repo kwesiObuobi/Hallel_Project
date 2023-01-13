330fec955127592f38b77c0f32008b1385160788
Samuel Obuobi Lartey <kwesiobuobi7@gmail.com>
2023-01-13 13:42:34 +0000

    Change '10' learning goals to 5

--- src/components/home_view/academics_page/learning_goals/learning_goals.jsx
+++ src/components/home_view/academics_page/learning_goals/learning_goals.jsx
@@ -5,7 +5,7 @@ const LearningGoals = () => {
   return (
     <section className="academics__learning-goals-section container">
         <h5 className="academics__learning-goals-h5">• OUR STUDENTS HAVE •</h5>
-        <h2 className="academics__learning-goals-h2">our 10 learning goals</h2>
+        <h2 className="academics__learning-goals-h2">our 5 learning goals</h2>
 
         <ol className="academics__learning-goals-list">
             <li className="academics__learning-goals-item">
