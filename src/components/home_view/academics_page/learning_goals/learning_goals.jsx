7bef745660a9955c20606b0c6cc60533ce7223ed
Samuel Obuobi Lartey <kwesiobuobi7@gmail.com>
2023-01-13 13:36:38 +0000

    Add Leadership as a learning goal

--- src/components/home_view/academics_page/learning_goals/learning_goals.jsx
+++ src/components/home_view/academics_page/learning_goals/learning_goals.jsx
@@ -4,9 +4,23 @@ import './learning_goals.css';
 const LearningGoals = () => {
   return (
     <section className="academics__learning-goals-section container">
-        <h5 className="academics__learning-goals-h5">• OUR STUDENTS HAVE THESE •</h5>
+        <h5 className="academics__learning-goals-h5">• OUR STUDENTS HAVE •</h5>
         <h2 className="academics__learning-goals-h2">our 10 learning goals</h2>
 
+        <ol className="academics__learning-goals-list">
+            <li className="academics__learning-goals-item">
+                <div className="academics__learning-goals-item-heading-row">
+                    <h3 className="academics__learning-goals-item-h3">Leadership</h3>
+                    <button className="academics__learning-goals-item-content-toggle">+</button>
+                </div>
+                <ul className="academics__learning-goals-item-description-box">
+                    <li classsName="academics__learning-goals-desc-box-item">A student from Hallel is proficient in leading and collaborating with others;</li>
+                    <li classsName="academics__learning-goals-desc-box-item">Has good interpersonal skills;</li>
+                    <li classsName="academics__learning-goals-desc-box-item">Is organized and able to plan</li>
+                </ul>
+            </li>
+            
+        </ol>
         
 
     </section>
