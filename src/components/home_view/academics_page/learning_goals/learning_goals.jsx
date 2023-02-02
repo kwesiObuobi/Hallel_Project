1c10636fd7bf12de8264bb25c54e435fb580754f
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-02-02 11:52:07 +0000

    Fixed Academic section bullets

--- src/components/home_view/academics_page/learning_goals/learning_goals.jsx
+++ src/components/home_view/academics_page/learning_goals/learning_goals.jsx
@@ -8,69 +8,71 @@ const LearningGoals = () => {
         <h5 className="academics__learning-goals-h5">• OUR STUDENTS HAVE •</h5>
         <h2 className="academics__learning-goals-h2">our 5 learning goals</h2>
 
-        <ol className="academics__learning-goals-list">
-            <li className="academics__learning-goals-item">
-                <div className="academics__learning-goals-item-heading-row">
-                    <h3 className="academics__learning-goals-item-h3">Leadership</h3>
-                    {/* <button className="academics__learning-goals-item-content-toggle">+</button> */}
-                    <HiChevronDown className='academics__learning-goals-item-content-toggle'/>
-                </div>
-                <ul className="academics__learning-goals-item-description-box">
-                    <li classsName="academics__learning-goals-desc-box-item">A student from Hallel is proficient in leading and collaborating with others;</li>
-                    <li classsName="academics__learning-goals-desc-box-item">Has good interpersonal skills;</li>
-                    <li classsName="academics__learning-goals-desc-box-item">Is organized and able to plan</li>
-                </ul>
-            </li>
+        <div className="academics__learning-goals-list-container">
+            <ol className="academics__learning-goals-list">
+                <li className="academics__learning-goals-item">
+                    <div className="academics__learning-goals-item-heading-row">
+                        <h3 className="academics__learning-goals-item-h3">Leadership</h3>
+                        {/* <button className="academics__learning-goals-item-content-toggle">+</button> */}
+                        <HiChevronDown className='academics__learning-goals-item-content-toggle'/>
+                    </div>
+                    <ul className="academics__learning-goals-item-description-box">
+                        <li classsName="academics__learning-goals-desc-box-item">A student from Hallel is proficient in leading and collaborating with others;</li>
+                        <li classsName="academics__learning-goals-desc-box-item">Has good interpersonal skills;</li>
+                        <li classsName="academics__learning-goals-desc-box-item">Is organized and able to plan</li>
+                    </ul>
+                </li>
 
-            <li className="academics__learning-goals-item">
-                <div className="academics__learning-goals-item-heading-row">
-                    <h3 className="academics__learning-goals-item-h3">Professionalism</h3>
-                    <HiChevronDown className='academics__learning-goals-item-content-toggle'/>
-                </div>
-                <ul className="academics__learning-goals-item-description-box">
-                    <li classsName="academics__learning-goals-desc-box-item">A student from Hallel is proficient in leading and collaborating with others;</li>
-                    <li classsName="academics__learning-goals-desc-box-item">Has good interpersonal skills;</li>
-                    <li classsName="academics__learning-goals-desc-box-item">Is organized and able to plan</li>
-                </ul>
-            </li>
+                <li className="academics__learning-goals-item">
+                    <div className="academics__learning-goals-item-heading-row">
+                        <h3 className="academics__learning-goals-item-h3">Professionalism</h3>
+                        <HiChevronDown className='academics__learning-goals-item-content-toggle'/>
+                    </div>
+                    <ul className="academics__learning-goals-item-description-box">
+                        <li classsName="academics__learning-goals-desc-box-item">A student from Hallel is proficient in leading and collaborating with others;</li>
+                        <li classsName="academics__learning-goals-desc-box-item">Has good interpersonal skills;</li>
+                        <li classsName="academics__learning-goals-desc-box-item">Is organized and able to plan</li>
+                    </ul>
+                </li>
 
-            <li className="academics__learning-goals-item">
-                <div className="academics__learning-goals-item-heading-row">
-                    <h3 className="academics__learning-goals-item-h3">Ethics</h3>
-                    <HiChevronDown className='academics__learning-goals-item-content-toggle'/>
-                </div>
-                <ul className="academics__learning-goals-item-description-box">
-                    <li classsName="academics__learning-goals-desc-box-item">A student from Hallel is proficient in leading and collaborating with others;</li>
-                    <li classsName="academics__learning-goals-desc-box-item">Has good interpersonal skills;</li>
-                    <li classsName="academics__learning-goals-desc-box-item">Is organized and able to plan</li>
-                </ul>
-            </li>
+                <li className="academics__learning-goals-item">
+                    <div className="academics__learning-goals-item-heading-row">
+                        <h3 className="academics__learning-goals-item-h3">Ethics</h3>
+                        <HiChevronDown className='academics__learning-goals-item-content-toggle'/>
+                    </div>
+                    <ul className="academics__learning-goals-item-description-box">
+                        <li classsName="academics__learning-goals-desc-box-item">A student from Hallel is proficient in leading and collaborating with others;</li>
+                        <li classsName="academics__learning-goals-desc-box-item">Has good interpersonal skills;</li>
+                        <li classsName="academics__learning-goals-desc-box-item">Is organized and able to plan</li>
+                    </ul>
+                </li>
 
-            <li className="academics__learning-goals-item">
-                <div className="academics__learning-goals-item-heading-row">
-                    <h3 className="academics__learning-goals-item-h3">Critical Thinking</h3>
-                    <HiChevronDown className='academics__learning-goals-item-content-toggle'/>
-                </div>
-                <ul className="academics__learning-goals-item-description-box">
-                    <li classsName="academics__learning-goals-desc-box-item">A student from Hallel is proficient in leading and collaborating with others;</li>
-                    <li classsName="academics__learning-goals-desc-box-item">Has good interpersonal skills;</li>
-                    <li classsName="academics__learning-goals-desc-box-item">Is organized and able to plan</li>
-                </ul>
-            </li>
+                <li className="academics__learning-goals-item">
+                    <div className="academics__learning-goals-item-heading-row">
+                        <h3 className="academics__learning-goals-item-h3">Critical Thinking</h3>
+                        <HiChevronDown className='academics__learning-goals-item-content-toggle'/>
+                    </div>
+                    <ul className="academics__learning-goals-item-description-box">
+                        <li classsName="academics__learning-goals-desc-box-item">A student from Hallel is proficient in leading and collaborating with others;</li>
+                        <li classsName="academics__learning-goals-desc-box-item">Has good interpersonal skills;</li>
+                        <li classsName="academics__learning-goals-desc-box-item">Is organized and able to plan</li>
+                    </ul>
+                </li>
 
-            <li className="academics__learning-goals-item">
-                <div className="academics__learning-goals-item-heading-row">
-                    <h3 className="academics__learning-goals-item-h3">Communication and Teamwork</h3>
-                    <HiChevronDown className='academics__learning-goals-item-content-toggle'/>
-                </div>
-                <ul className="academics__learning-goals-item-description-box">
-                    <li classsName="academics__learning-goals-desc-box-item">A student from Hallel is proficient in leading and collaborating with others;</li>
-                    <li classsName="academics__learning-goals-desc-box-item">Has good interpersonal skills;</li>
-                    <li classsName="academics__learning-goals-desc-box-item">Is organized and able to plan</li>
-                </ul>
-            </li>
+                <li className="academics__learning-goals-item">
+                    <div className="academics__learning-goals-item-heading-row">
+                        <h3 className="academics__learning-goals-item-h3">Communication and Teamwork</h3>
+                        <HiChevronDown className='academics__learning-goals-item-content-toggle'/>
+                    </div>
+                    <ul className="academics__learning-goals-item-description-box">
+                        <li classsName="academics__learning-goals-desc-box-item">A student from Hallel is proficient in leading and collaborating with others;</li>
+                        <li classsName="academics__learning-goals-desc-box-item">Has good interpersonal skills;</li>
+                        <li classsName="academics__learning-goals-desc-box-item">Is organized and able to plan</li>
+                    </ul>
+                </li>
 
-        </ol>
+            </ol>
+        </div>
         
 
     </section>
