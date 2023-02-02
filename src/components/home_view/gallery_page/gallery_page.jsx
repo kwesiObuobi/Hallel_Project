f10651651225a107bacf8ae6321e08139e669ca1
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-02-02 22:06:29 +0000

    Started to create modal for the gallery page

--- src/components/home_view/gallery_page/gallery_page.jsx
+++ src/components/home_view/gallery_page/gallery_page.jsx
@@ -13,15 +13,25 @@ const GalleryPage = () => {
       <div className="gallery-section container">
         <h3 className="gallery-section-h3">Clubs and Houses</h3>
         <div className="gallery-section-images">
-          {/* <img src="" alt="" className="gallery-section-image" /> */}
           <div className="gallery-section-img-item-box">
-            <img src="" alt="" className="gallery-section-img" />
+            <img src="https://www.cru.org/communities/hs/wp-content/uploads/sites/18/2020/03/Gathering-Students.jpg" alt="Inkoomsah" className="gallery-section-img" />
             <small className="gallery-section-image-desc">Inkoomsah Hall</small>
           </div>
 
-          <div className="gallery-section-img-item-box">1</div>
-          <div className="gallery-section-img-item-box">2</div>
-          <div className="gallery-section-img-item-box">3</div>
+          <div className="gallery-section-img-item-box">
+            <img src="https://knowledgeworks.org/wp-content/uploads/2016/07/Teacher-Working-300x200.jpg" alt="Inkoomsah" className="gallery-section-img" />
+            <small className="gallery-section-image-desc">Godsaye Simpson</small>
+          </div>
+
+          <div className="gallery-section-img-item-box">
+            <img src="https://www.cru.org/communities/hs/wp-content/uploads/sites/18/2020/03/Gathering-Students.jpg" alt="Inkoomsah" className="gallery-section-img" />
+            <small className="gallery-section-image-desc">Inkoomsah Hall</small>
+          </div>
+
+          <div className="gallery-section-img-item-box">
+            <img src="https://knowledgeworks.org/wp-content/uploads/2016/07/Teacher-Working-300x200.jpg" alt="Inkoomsah" className="gallery-section-img" />
+            <small className="gallery-section-image-desc">Godsaye Simpson</small>
+          </div>
         </div>
       </div>
 
