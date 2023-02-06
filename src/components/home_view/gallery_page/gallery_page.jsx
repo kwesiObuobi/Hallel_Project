552fd59f1fc67633b3f31d11c3f8f2588d67c58c
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-02-06 21:17:32 +0000

    Combined all gallery css files into one file

--- src/components/home_view/gallery_page/gallery_page.jsx
+++ src/components/home_view/gallery_page/gallery_page.jsx
@@ -1,11 +1,10 @@
 import React, {useState} from 'react'
-
 import './gallery_page.css'
 
 import galleryPageImg from '../../../assets/images/website_images/gallery.jpg'
 import PageImg from '../../shared/page_img/page_img'
-import GsModal from './gs_modal/gs_modal'
-import GsListItem from './gs_list_item/gs_list_item'
+import GsModal from './gs_modal'
+import GsListItem from './gs_list_item'
 
 const GalleryPage = () => {
 
@@ -45,7 +44,6 @@ const GalleryPage = () => {
         <h3 className="gallery-section-h3">Clubs and Houses</h3>
         <div className="gallery-section-images">
 
-
           {
             clubsAndHouses.map(({title, img_url}, id) => {
               return (
@@ -76,7 +74,6 @@ const GalleryPage = () => {
       <div className="gallery-section container">
         <h3 className="gallery-section-h3">Clubs and Houses</h3>
         <div className="gallery-section-images">
-          {/* <img src="" alt="" className="gallery-section-image" /> */}
           <div className="gallery-section-img-item-box"></div>
 
           <div className="gallery-section-img-item-box">1</div>
--- src/components/home_view/gallery_page/gs_modal.jsx
+++ src/components/home_view/gallery_page/gs_modal.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './gs_modal.css';
 
 const GsModal = ({open, closeModal}) => {
     if (open) {
