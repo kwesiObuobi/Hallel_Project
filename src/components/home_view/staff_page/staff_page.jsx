277f390da2d39f95d503b3474bf452ba7f8b5c2e
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-02-08 11:46:30 +0000

    Added skeleton of how a staff card will look like

--- src/components/home_view/staff_page/staff_page.jsx
+++ src/components/home_view/staff_page/staff_page.jsx
@@ -18,7 +18,44 @@ const StaffPage = () => {
           <h3 className="staff-section_desc_h3">The team at hallel Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, perferendis</h3>
           <p className="staff-section_desc_p">We're driven by the idea that Lorem ipsum dolor sit amet consectetur adipisicing.</p>
         </div>
-        
+
+        <div className="staff-section_group">
+          <h3 className="staff-section_group_h3">Leadership</h3>
+          
+          <div className="staff-section_group_items">
+            <div className="staff-section_group_item">
+              <img src="" alt="" className="staff-section_group_item_img" />
+              <p className="staff-section_group_item_p">Name Goes here long name yes</p>
+              <small className="staff-section_group_item_role">Head Mistress</small>
+            </div>
+
+            <div className="staff-section_group_item">
+              <img src="" alt="" className="staff-section_group_item_img" />
+              <p className="staff-section_group_item_p">Name Goes here long name yes</p>
+              <small className="staff-section_group_item_role">Head Mistress</small>
+            </div>
+
+            <div className="staff-section_group_item">
+              <img src="" alt="" className="staff-section_group_item_img" />
+              <p className="staff-section_group_item_p">Name Goes here long name yes</p>
+              <small className="staff-section_group_item_role">Head Mistress</small>
+            </div>
+
+            <div className="staff-section_group_item">
+              <img src="" alt="" className="staff-section_group_item_img" />
+              <p className="staff-section_group_item_p">Name Goes here long name yes</p>
+              <small className="staff-section_group_item_role">Head Mistress</small>
+            </div>
+
+            <div className="staff-section_group_item">
+              <img src="" alt="" className="staff-section_group_item_img" />
+              <p className="staff-section_group_item_p">Name Goes here long name yes</p>
+              <small className="staff-section_group_item_role">Head Mistress</small>
+            </div>
+          </div>
+          
+        </div>
+
       </section>
 
     </>
