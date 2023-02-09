ff2f1770ab3abaced78eef96440438e7f29289d0
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-02-09 23:51:28 +0000

    Began to style the student spotlight section

--- src/components/home_view/student_page/student_page.jsx
+++ src/components/home_view/student_page/student_page.jsx
@@ -9,6 +9,80 @@ const StudentPage = () => {
   return (
     <>
       <PageImg img_url={studentPageImg} title="Student" />
+
+      <section className="home-student container">
+        <h5 className="staff-section__h5">• LET'S RECOGNISE THEM •</h5>
+        <h2 className="staff-section__h2">students in the spotlight</h2>
+
+        <div className="home-student-spotlights">
+
+          <div className="home-student-spotlight">
+            <h3 className="home-student-spotlight-name">Best Student (Class 1)</h3>
+            <div className="home-student-spotlight-img-box">
+              <img src="https://pbs.twimg.com/media/D2qtOY8WkAEnGd1?format=jpg&name=4096x4096" alt="" className="home-student-spotlight-img" />
+            </div>
+            <h4 className="home-student-spotlight-student">Emmanuel Kweku Godsaye-Simpson</h4>
+            <small className="home-student-spotlight-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum itaque ipsum magni sint soluta. Distinctio, tempore accusamus deserunt eos eligendi dolorum repellat repellendus porro, aliquam cum dolorem accusantium a velit, debitis fugit ullam sunt quasi omnis fugiat obcaecati aspernatur labore! Doloribus beatae nesciunt velit! Aut ipsa laborum eius animi fuga?</small>
+          </div>
+
+          <div className="home-student-spotlight">
+            <h3 className="home-student-spotlight-name">Best Student (Class 2)</h3>
+            <div className="home-student-spotlight-img-box">
+              <img src="https://pbs.twimg.com/media/D2qtOY8WkAEnGd1?format=jpg&name=4096x4096" alt="" className="home-student-spotlight-img" />
+            </div>
+            <h4 className="home-student-spotlight-student">Emmanuel Kweku Godsaye-Simpson</h4>
+            <small className="home-student-spotlight-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum itaque ipsum magni sint soluta. Distinctio, tempore accusamus deserunt eos eligendi dolorum repellat repellendus porro, aliquam cum dolorem accusantium a velit, debitis fugit ullam sunt quasi omnis fugiat obcaecati aspernatur labore! Doloribus beatae nesciunt velit! Aut ipsa laborum eius animi fuga?</small>
+          </div>
+
+          <div className="home-student-spotlight">
+            <h3 className="home-student-spotlight-name">Best Student (Class 3)</h3>
+            <div className="home-student-spotlight-img-box">
+              <img src="https://pbs.twimg.com/media/D2qtOY8WkAEnGd1?format=jpg&name=4096x4096" alt="" className="home-student-spotlight-img" />
+            </div>
+            <h4 className="home-student-spotlight-student">Emmanuel Kweku Godsaye-Simpson</h4>
+            <small className="home-student-spotlight-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum itaque ipsum magni sint soluta. Distinctio, tempore accusamus deserunt eos eligendi dolorum repellat repellendus porro, aliquam cum dolorem accusantium a velit, debitis fugit ullam sunt quasi omnis fugiat obcaecati aspernatur labore! Doloribus beatae nesciunt velit! Aut ipsa laborum eius animi fuga?</small>
+          </div>
+
+          <div className="home-student-spotlight">
+            <h3 className="home-student-spotlight-name">Best Student (Class 4)</h3>
+            <div className="home-student-spotlight-img-box">
+              <img src="https://pbs.twimg.com/media/D2qtOY8WkAEnGd1?format=jpg&name=4096x4096" alt="" className="home-student-spotlight-img" />
+            </div>
+            <h4 className="home-student-spotlight-student">Emmanuel Kweku Godsaye-Simpson</h4>
+            <small className="home-student-spotlight-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum itaque ipsum magni sint soluta. Distinctio, tempore accusamus deserunt eos eligendi dolorum repellat repellendus porro, aliquam cum dolorem accusantium a velit, debitis fugit ullam sunt quasi omnis fugiat obcaecati aspernatur labore! Doloribus beatae nesciunt velit! Aut ipsa laborum eius animi fuga?</small>
+          </div>
+
+          <div className="home-student-spotlight">
+            <h3 className="home-student-spotlight-name">Best Student (Class 5)</h3>
+            <div className="home-student-spotlight-img-box">
+              <img src="https://pbs.twimg.com/media/D2qtOY8WkAEnGd1?format=jpg&name=4096x4096" alt="" className="home-student-spotlight-img" />
+            </div>
+            <h4 className="home-student-spotlight-student">Emmanuel Kweku Godsaye-Simpson</h4>
+            <small className="home-student-spotlight-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum itaque ipsum magni sint soluta. Distinctio, tempore accusamus deserunt eos eligendi dolorum repellat repellendus porro, aliquam cum dolorem accusantium a velit, debitis fugit ullam sunt quasi omnis fugiat obcaecati aspernatur labore! Doloribus beatae nesciunt velit! Aut ipsa laborum eius animi fuga?</small>
+          </div>
+
+          <div className="home-student-spotlight">
+            <h3 className="home-student-spotlight-name">Best Student (Class 6)</h3>
+            <div className="home-student-spotlight-img-box">
+              <img src="https://pbs.twimg.com/media/D2qtOY8WkAEnGd1?format=jpg&name=4096x4096" alt="" className="home-student-spotlight-img" />
+            </div>
+            <h4 className="home-student-spotlight-student">Emmanuel Kweku Godsaye-Simpson</h4>
+            <small className="home-student-spotlight-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum itaque ipsum magni sint soluta. Distinctio, tempore accusamus deserunt eos eligendi dolorum repellat repellendus porro, aliquam cum dolorem accusantium a velit, debitis fugit ullam sunt quasi omnis fugiat obcaecati aspernatur labore! Doloribus beatae nesciunt velit! Aut ipsa laborum eius animi fuga?</small>
+          </div>
+
+          <div className="home-student-spotlight">
+            <h4 className="home-student-spotlight-name">Best Student (Class 7)</h4>
+            <div className="home-student-spotlight-img-box">
+              <img src="https://pbs.twimg.com/media/D2qtOY8WkAEnGd1?format=jpg&name=4096x4096" alt="" className="home-student-spotlight-img" />
+            </div>
+            <h4 className="home-student-spotlight-student">Emmanuel Kweku Godsaye-Simpson</h4>
+            <small className="home-student-spotlight-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum itaque ipsum magni sint soluta. Distinctio, tempore accusamus deserunt eos eligendi dolorum repellat repellendus porro, aliquam cum dolorem accusantium a velit, debitis fugit ullam sunt quasi omnis fugiat obcaecati aspernatur labore! Doloribus beatae nesciunt velit! Aut ipsa laborum eius animi fuga?</small>
+          </div>
+
+
+
+        </div>
+      </section>
     </>
   )
 }
