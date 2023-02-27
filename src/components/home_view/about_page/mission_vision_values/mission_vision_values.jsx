1619b170c51dbc54bca9064dde0296c4a56bcd61
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-02-27 21:45:15 +0000

    Update mission and vision on the about page

--- src/components/home_view/about_page/mission_vision_values/mission_vision_values.jsx
+++ src/components/home_view/about_page/mission_vision_values/mission_vision_values.jsx
@@ -4,14 +4,6 @@ import './mission_vision_values.css';
 
 const MissionVisionValues = () => {
   return (
-    // mvv = mission_vision_values
-    // <div className="mvv-container-outer-box">
-    //   <section className="mvv-container">
-    //   <h5> • WHERE WE ARE HEADED • </h5>
-    //   <h2>mission | vision | values</h2>
-    //   </section>
-    // </div>
-
     
     <section className="mvv-container">
       <h5 className="mvv-h5"> • WHERE WE'RE HEADED • </h5>
@@ -19,12 +11,14 @@ const MissionVisionValues = () => {
 
       <div className="container mvv-sub-heading">
         <h3 className="mvv-sub-heading-h3">Our Mission</h3>
-        <p className="mvv-sub-heading-p">Vulputate enim nulla aliquet porttitor lacus luctus. Mauris in aliquam sem fringilla. Urna porttitor rhoncus dolor purus.</p>
+        <p className="mvv-sub-heading-p">To provide high quality eduacation in an exciting, innovative environment that will
+       enhance learning with the concerns for the child's intellectual, spiritual, social and emotional development at heart.</p>
       </div>
 
       <div className="container mvv-sub-heading">
         <h3 className="mvv-sub-heading-h3">Our Vision</h3>
-        <p className="mvv-sub-heading-p">Vulputate enim nulla aliquet porttitor lacus luctus. Mauris in aliquam sem fringilla. Urna porttitor rhoncus dolor purus.</p>
+        <p className="mvv-sub-heading-p">To inspire and equip children with skills to build values for a solid foundation that will enable them reach their divine 
+        potential. This will be accomplished through personal enhancement of children from an early age through the exposure they will have whilst in our custody.</p>
       </div>
 
       <div className="container mvv-sub-heading">
