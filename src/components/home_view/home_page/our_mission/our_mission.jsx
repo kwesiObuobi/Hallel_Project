3ab1132620e476df4e4965108f501abefd9dcfcc
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-02-27 21:40:08 +0000

    Update mission on the landing page

--- src/components/home_view/home_page/our_mission/our_mission.jsx
+++ src/components/home_view/home_page/our_mission/our_mission.jsx
@@ -6,8 +6,12 @@ const OurMission = () => {
     <section className="container mission-container">
       <h5> • GET TO KNOW • </h5>
       <h2>our mission</h2>
-      <h3 className="mission__h3">We have an explicit mission to prepare entirely <br/>trustworthy, entrepreneurial leaders in Africa;</h3>
-      <p className="mission__p">to raise within students the critical thinking skills, the concern for others, <br/>and the overall courage it will take to transform the continent positively. <br/>The "better world" starts with us!</p>
+      <h3 className="mission__h3">To provide high quality eduacation in an exciting, innovative environment that will
+       enhance learning with the concerns for the child's intellectual, spiritual, social and emotional development at heart.</h3>
+      {/* <p className="mission__p">to raise within students the critical thinking skills, the concern for others, <br/>and the overall courage it will take to transform the continent positively. <br/>The "better world" starts with us!</p> */}
+
+      {/* <h3 className="mission__h3">We have an explicit mission to prepare entirely <br/>trustworthy, entrepreneurial leaders in Africa;</h3> */}
+      {/* <p className="mission__p">to raise within students the critical thinking skills, the concern for others, <br/>and the overall courage it will take to transform the continent positively. <br/>The "better world" starts with us!</p> */}
     </section>
   )
 }
